dbdab9b7312d19a134b02d1f800a4734f6e4d4a8
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-04 19:19:43 -0300

    Update

--- Methodos, gerenic, interface/metodos/cmd/main.go
+++ Methodos, gerenic, interface/metodos/cmd/main.go
@@ -2,28 +2,40 @@ package main
 
 import (
 	"estoque/internal/models"
+	"estoque/internal/services"
 	"fmt"
 )
 
 
 func main() {
-	item1 := models.Item{
-		ID: 1,
-		Name: "Ps5",
-		Quantity: 2,
-		Price: 1200.00,
+	// Slice de itens da struct Item
+	itens := []models.Item {
+		{ID: 1, Name: "Teste 1", Quantity: 8, Price: 122.00},
+		{ID: 2, Name: "Teste 2", Quantity: 6, Price: 390.99},
+		{ID: 3, Name: "Teste 3", Quantity: 2, Price: 149.76},
 	}
 
-	fmt.Println(item1.Info())
+	// Faz referência a struct Estoque
+	estoque := services.NewEstoque()
 
-	item2 := models.Item{
-		ID: 2,
-		Name: "Ps4",
-		Quantity: 4,
-		Price: 800.00,
+	// Percorrer com o index para adicionar todos os itens do Slice 'itens'
+	// o segundo valor de range seria um models.Item que é uma struct
+	for index := range itens {
+		estoque.AddItem(itens[index])
 	}
 
-	fmt.Println(item2.Info())
+	// Ou pode fazer dessa forma (a do curso)
+	// for _, item := range itens {
+	// 	err := estoque.AddItem(item)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		continue
+	// 	}
+	// }
 
-	
+	// fmt.Println(estoque.ListItens())
+	for _, item := range estoque.ListItens() {
+		fmt.Printf("\n ID: %d | Item: %s | Quantidade: %d| Preço: %.2f",
+	item.ID, item.Name, item.Quantity, item.Price)
+	}
 }
--- Methodos, gerenic, interface/metodos/internal/services/estoque.go
+++ Methodos, gerenic, interface/metodos/internal/services/estoque.go
@@ -1,6 +1,10 @@
 package services
 
-import "estoque/internal/models"
+import (
+	"estoque/internal/models"
+	"fmt"
+	"strconv"
+)
 
 type Estoque struct {
 	itens map[string]models.Item
@@ -8,10 +12,33 @@ type Estoque struct {
 }
 
 
-// Passar informações do estoque por valor
+// Passar informações do estoque por referência
 func NewEstoque() *Estoque {
 	return &Estoque{
 		itens: make(map[string]models.Item),
 		logs: []models.Log{},
 	}
+}
+
+
+// Método para adicionar Estoque
+func (e *Estoque) AddItem(item models.Item) error {
+	if item.Quantity <= 0 {
+		return fmt.Errorf("erro ao adicionar o produto, a quantidade não pode ser menor ou igual a zero Qtd atual: %d ", item.Quantity)
+	}
+	existingItem, exists := e.itens[strconv.Itoa(item.ID)]
+	if exists {
+		item.Quantity += existingItem.Quantity
+	}
+	e.itens[strconv.Itoa(item.ID)] = item
+	return nil
+}
+
+// Método para Listar Itens do Estoque
+func (e *Estoque) ListItens() []models.Item {
+	var itensList []models.Item // Lista dos itens (slice) de (maps) que vamos receber na main
+	for _, item := range e.itens {
+		itensList = append(itensList, item)
+	}
+	return itensList
 }
