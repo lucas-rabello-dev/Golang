a5f902c2d32a2eee28b561d8d9d46887dfe52887
Lucas Rabello <lucasrabello2020@gmail.com>
2025-08-14 17:58:18 -0300

    Update concurrancy.go

--- concorrência/exemplo 3/concurrancy.go
+++ concorrência/exemplo 3/concurrancy.go
@@ -16,7 +16,7 @@ func main() {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			for j := 0; j < 1000000000; j++ { // aqui 5 só pra não demorar
+			for j := 0; j < 1000000000; j++ {
 				nums[i]++
 			}
 			fmt.Printf("num%d: %d\n", i+1, nums[i])
